3f686cff852c99130c921b0a302c81ef5d0f2112
nidhiambatkar026 <nidhiambatkar@gmail.com>
2025-06-20 14:10:38 +0530

    more logics created

--- src/ATM_aiml.java
+++ src/ATM_aiml.java
@@ -2,8 +2,11 @@ import java.util.*;
 class Account_AIML{
     private int CNo;
     private int b;
+
+    double CB = 2000;
+
     void set_Customer_Number(int a){
-       CNo = a;
+       CNo = a;//setter method(the value is assigned)
     }
     void set_PIN_Number(int b){
         this.b = b;
@@ -14,12 +17,18 @@ class Account_AIML{
     int get_PIN_Number(){
         return b;
     }
+    void getCurrentBalance(){
+       System.out.println("Current account Balance: " + CB);
+    }
 }
 class Option_Menu_AIML extends Account_AIML{
     Scanner sc = new Scanner(System.in);
     HashMap<Integer,Integer> data =new HashMap();
+    
     void get_Login(){
-
+       
+      do{
+        try {
        data.put(11111, 111);
        data.put(11112, 222);
        data.put(11113, 333);
@@ -31,26 +40,129 @@ class Option_Menu_AIML extends Account_AIML{
        data.put(11119, 999);
        data.put(11110, 101);
 
-
        System.out.println("Welcome To the ATM");
-       System.out.println("Enter the Customer Number: ");
+       System.out.print("\nEnter the Customer Number : ");
        set_Customer_Number(sc.nextInt());
-       System.out.println("Enter the PIN Number: ");
+       System.out.print("\nEnter the PIN Number : ");
        set_PIN_Number(sc.nextInt());
 
        int P= get_Customer_Number();
        int Q= get_PIN_Number();
 
        if(data.containsKey(P) && data.get(P)==Q){
-          System.out.println("Login Successful");
+          get_Account_Type();
        }
        else{
           System.out.println("Incorrect Customer Number or PIN Number");
        }
+      } catch (InputMismatchException obj) {
+        System.out.println("\nPlease Enter only Numbers");
+        System.out.println("Characters and Symbols are not allowed");
+        sc.next();
+        get_Login();
+      }
+      }while(true);
+    }
 
+
+    void get_Account_Type(){
+      System.out.println("\nSelect the Account type you want to access: ");
+      System.out.println("\nType 1: Current Account");
+      System.out.println("Type 2: Saving Account");
+      System.out.println("Type 3: Exit");
+      System.out.println("\nChoice: ");
+      Scanner sc = new Scanner(System.in);
+      int Ch = sc.nextInt();
+      switch (Ch) {
+        case 1:
+            getCurrent();
+            System.out.println("Current Account");
+            break;
+
+        case 2: 
+            getSaving();
+            System.out.println("Savings Account");
+            break;
+
+        case 3: 
+            System.out.println("\nThank you for Visiting");
+            System.out.println("Visit Again");
+            break;
+      
+        default:
+            System.out.println("Invalid Choice");
+            System.out.println("Please Enter the Valid Choice: ");
+            get_Account_Type();
+            break;
+      }
+    }
+    void getCurrent(){
+        System.out.println("\nCURRENT ACCOUNT");
+        System.out.println("\nType 1: Check Balance");
+        System.out.println("\nType 2: Withdraw Money");
+        System.out.println("\nType 3: Deposit Money");
+        System.out.println("\nExit");
+        System.out.print("\nChoice : ");
+        int ch = sc.nextInt();
+        switch (ch) {
+            case 1:
+                getCurrentBalance();
+                break;
+
+            case 2: 
+                System.out.println("Withdraw Money");
+                break;
+
+            case 3: 
+                System.out.println("Deposit Money");
+                break;
+
+            case 4:
+                System.out.println("\nThank you for Visiting");
+                System.out.println("Visit Again");
+                break;
+        
+            default:
+                System.out.println("Invalid Choice");
+                System.out.println("Please Enter the Valid Choice: ");
+                get_Account_Type();
+                break;
+        }
+    }
+    void getSaving(){
+        System.out.println("\nSAVINGS ACCOUNT");
+        System.out.println("\nType 1: Check Balance");
+        System.out.println("\nType 2: Withdraw Money");
+        System.out.println("\nType 3: Deposit Money");
+        System.out.println("\nExit");
+        System.out.print("\nChoice : ");
+        int ch = sc.nextInt();
+        switch (ch) {
+            case 1:
+                getCurrentBalance();
+                break;
+
+            case 2: 
+                System.out.println("Withdraw Money");
+                break;
+
+            case 3: 
+                System.out.println("Deposit Money");
+                break;
+
+            case 4:
+                System.out.println("\nThank you for Visiting");
+                System.out.println("Visit Again");
+                break;
+        
+            default:
+                System.out.println("Invalid Choice");
+                System.out.println("Please Enter the Valid Choice: ");
+                get_Account_Type();
+                break;
+        }
     }
 }
-
 public class ATM_aiml extends Option_Menu_AIML{
     public static void main(String args[]){
         Option_Menu_AIML obj = new Option_Menu_AIML();
